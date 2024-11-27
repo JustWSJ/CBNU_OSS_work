368568ff27e4dfb698ae3a4d547e34a7864e09cf
whro1031 <whrowhro.joge@gmail.com>
2024-11-27 17:20:25 +0900

    delete startgame() in game.c 만들 필요없음

--- dungeon/game.c
+++ dungeon/game.c
@@ -3,14 +3,6 @@
 #include "dungeon.h"
 #include "save.h"
 
-void startNewGame() {
-    Dungeon dungeon;
-    generateDungeon(&dungeon, 1); // 첫 번째 레벨 던전 생성
-    printDungeon(&dungeon);
-
-    saveGame("savegame.dat", &dungeon);  // 게임 저장
-}
-
 void loadExistingGame() {
     Dungeon dungeon;
     loadGame("savegame.dat", &dungeon); // 저장된 게임 불러오기
--- dungeon/game.h
+++ dungeon/game.h
@@ -1,7 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
 
-void startNewGame();
 void loadExistingGame();
 
 #endif
