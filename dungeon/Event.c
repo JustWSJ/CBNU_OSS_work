e752d25b7158b7c1232d6fd0a67f9d9201d07d11
JustWSJ <dtw2191@gmail.com>
2024-12-02 18:19:48 +0900

    adjustment Event.c

--- dungeon/Event.c
+++ dungeon/Event.c
@@ -9,21 +9,23 @@
 
 //  이벤트 이름
 /*
-R = road
-W = Wall
-B = battle
-K = boss battle
+B(강제 전투)
+S(칸 탐색 후 전투)
+K(보스)
+F(랜덤 이벤트)
+I(지정 이벤트)
 */
-const char *Event_name[4] = {"R", "W", "B", "K"};
+
+const char *Event_name[5] = {"B", "S", "K", "F", "I"};
 
 void callEvent(char flag){
     if(flag == Event_name[0]){
-
+        srand(time(NULL));
+        battle(getPlayer(), getMonster(rand()%5));
     } else if(flag == Event_name[1]) {
 
     } else if(flag == Event_name[2]) {
-        srand(time(NULL));
-        battle(getPlayer(), getMonster(rand()%5));
+
     } else if(flag == Event_name[3]) {
 
     }
