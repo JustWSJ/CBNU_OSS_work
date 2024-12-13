7aefeba1d9ed924aa50fa71eb2c2880910441f1a
l6j6h6lgh <l6j6h6lgh@gmail.com>
2024-12-13 18:23:23 +0900

    휴식 기능 추가

--- dungeon/hotel.h
+++ dungeon/hotel.h
@@ -1,7 +1,14 @@
 #ifndef HOTEL_H
 #define HOTEL_H
 
+#include <stdbool.h>
+#include "character.h"
 
+// 함수 선언
+void createPortalToTown(const char* dungeonName, int currentFloor);
+void restAtHotel(Character* player);
+bool checkFloorClear(int currentFloor);
+void saveProgress(const char* dungeonName, int clearedFloor);
+Character loadProgress(void);
 
-
-#endif
+#endif // HOTEL_H
