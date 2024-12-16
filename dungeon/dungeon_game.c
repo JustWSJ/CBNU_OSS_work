804e9931a787502b3988fb0003810a24dad2a19b
whro1031 <whrowhro.joge@gmail.com>
2024-12-16 16:51:00 +0900

    코드정리 dungeon_game.c

--- dungeon/dungeon_game.c
+++ dungeon/dungeon_game.c
@@ -10,39 +10,15 @@
 
 int mainMenu(void);
 void InitializeSystem(void);
+void StartingOption(int, Character *);
+
 
 int main() {
     InitializeSystem();
     Character *player = getPlayer();
     int selectedOption = mainMenu(); // 메인 메뉴 표시 및 선택값 반환
-    //  select newgame
-    if (selectedOption == 1) {
-        showStatusAllocation();
-        clearScreen();
-
-        //printf("%d %d %d %d %d %d\n", player->health, player->strength, player->agility, player->intelligence, player->sensory, player->luck);
-        saveStatus(player);
-    }
-    //  select loadgame
-    else if (selectedOption == 2) {
-        // Load Game 처리
-        printf("\n\nLoading Game...\n");
-        wait(); // 지우기
-        if (loadStatus(player) != -1){
-            printf("%d %d %d %d %d %d\n", player->health, player->strength, player->agility, player->intelligence, player->sensory, player->luck);
-        } else {
-            printf("cat find savefile.\nstart new game.\n");
-            wait();
-            showStatusAllocation();
-            saveStatus(player);
-            clearScreen();
-        }
-        
-    }
-    //  select exit
-    else if (selectedOption == 3) {
-        exit(0);
-    }
+    StartingOption(selectedOption, player);
+   
     printf("설정한 스탯 저장하고 마을 화면 넘어가기.\nPress Enter.\n"); //여기가 while문 0층이 되어야함.
     wait(); // 지우기
     //Dungeon();
@@ -60,4 +36,34 @@ void InitializeSystem(void){
     system("COLOR 0F");           // 배경: 검정, 글자색: 흰색
     system("mode con: cols=52 lines=30");       // 콘솔 크기 설정
     system("title Dungeon Game"); // 콘솔 창 제목 설정
+}
+
+void StartingOption(int selectedOption, Character *player){
+ //  select newgame
+    if (selectedOption == 1) {
+        showStatusAllocation();
+        saveStatus(player);
+        clearScreen();
+    }
+    //  select loadgame
+    else if (selectedOption == 2) {
+        // Load Game 처리
+        printf("\n\nLoading Game...\n");
+        wait(); // 지우기
+        if (loadStatus(player) != -1){
+            printf("Found savefile.\n");
+            //테스트용
+            printf("%d %d %d %d %d %d\n", player->health, player->strength, player->agility, player->intelligence, player->sensory, player->luck);
+        } else {
+            printf("Can't find savefile.\nStarting a new game. ▼\n");
+            wait();
+            showStatusAllocation();
+            saveStatus(player);
+            clearScreen();
+        }
+    }
+    //  select exit
+    else if (selectedOption == 3) {
+        exit(0);
+    }
 }
