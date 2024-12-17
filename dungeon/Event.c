03ac2ff582bd15344734bfb47884420bfe5a23cd
JustWSJ <dtw2191@gmail.com>
2024-12-17 20:56:42 +0900

    edit Event.c

--- dungeon/Event.c
+++ dungeon/Event.c
@@ -6,6 +6,7 @@
 #include "monster.h"
 #include "battle.h"
 #include "shop.h"
+#include "items.h"
 
 
 // *B (Battle): 강제 전투
@@ -19,7 +20,11 @@
 // 0W (Wall): 벽. 막힘
 // *X (eXit): 출구
 
-void callEvent(char flag){
+void solvePuzzle(Character* player);
+void unlockBox(Character* player);
+void luckybox(Character* player);
+
+void callEvent(char flag, int floor){
     switch(flag){
         case 'D':
         //  call dungeon
@@ -32,7 +37,7 @@ void callEvent(char flag){
         case 'B':
         //  call battle
         srand(time(NULL));
-        battle(getPlayer(), getMonster(rand()%5));
+        battle(getPlayer(), getMonster((rand()%4 + 1) + ((floor / 10) * 10)));
         return;
 
         case 'S':
@@ -41,10 +46,20 @@ void callEvent(char flag){
 
         case 'K':
         //  call boss battle
+        battle(getPlayer(), 1000 + (floor / 10));
         return;
 
         case 'F':
         //  call randum event
+        srand(time(NULL));
+        int r = rand()%33;
+        if(r > 23){
+            solvepuzzle(getPlayer());
+        }else if(r > 13){
+            unlockBox(getplayer());
+        } else {
+            luckybox(getPlayer());
+        }
         return;
 
         case 'I':
@@ -54,4 +69,45 @@ void callEvent(char flag){
         default:
         return;
     }
+}
+
+void solvePuzzle(Character* player) {
+    srand(time(NULL));
+    int puzzleDifficulty = rand()%100 - player->intelligence;  // 퍼즐 난이도
+    printf("퍼즐 난이도: %d\n", (puzzleDifficulty / 10));
+    printf("당신의 지능 수치: %d\n", player->intelligence);
+
+    if (player->intelligence >= puzzleDifficulty) {
+        printf("퍼즐을 성공적으로 풀었습니다!\n");
+    } else {
+        printf("지능이 부족하여 퍼즐을 풀지 못했습니다.\n");
+    }
+}
+
+// 자물쇠 해제 함수
+void unlockBox(Character* player) {
+    srand(time(NULL));
+    int lockDifficulty = rand()%100 - player->sensory;  // 자물쇠 난이도
+    printf("자물쇠 난이도: %d\n", (lockDifficulty / 10));
+    printf("당신의 감각 수치: %d\n", player->sensory);
+
+    if (player->sensory >= lockDifficulty) {
+        printf("자물쇠를 성공적으로 열었습니다!\n");
+    } else {
+        printf("감각이 부족하여 자물쇠를 열지 못했습니다.\n");
+    }
+}
+
+void luckybox(Character* player){
+    srand(time(NULL));
+    int luckybox = rand()%100 - player->luck;
+    printf("꽝 확률: %d\n", (luckybox / 10));
+    printf("당신의 운 수치: %d\n", player->sensory);
+
+    if (player->luck >= luckybox) {
+        printf("당첨 되었습니다!\n");
+    } else {
+        printf("꽝!\n");
+    }
+
 }
