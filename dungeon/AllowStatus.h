236ce037afdd25468a6b6c27fe6a2e9a957ec347
whro1031 <whrowhro.joge@gmail.com>
2024-12-17 15:43:24 +0900

    delete AllowStatus.h 이미 있는 기능임

--- dungeon/AllowStatus.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALLOWSTATUS_H
-#define ALLOWSTATUS_H
-#include "Character.h"
-
-void updateStat(Character *player, int statID, int delta);
-int getStatValue(Character *player, int statID);
-void showStatusAllocation(Character *player);
-
-#endif
