a88a45f89c60f5fcc20160ecf3730c0bf3a99d6d
whro1031 <whrowhro.joge@gmail.com>
2024-12-17 15:42:24 +0900

    fix maxHealth -> max_health

--- dungeon/hotel.c
+++ dungeon/hotel.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include "Character.h"
 #include "hotel.h"
-#include "save.h"
+#include "saveLoad.h"
 
 // 호텔 방문: 체력 회복 및 진행 상황 저장
 void visitHotel(Character* player) {
     printf("\n[호텔 방문]\n");
 
     // 체력 회복
-    player->health = player->maxHealth;
-    printf("체력이 완전히 회복되었습니다! (현재 체력: %d/%d)\n", player->health, player->maxHealth);
+    player->health = player->max_health;
+    printf("체력이 완전히 회복되었습니다! (현재 체력: %d/%d)\n", player->health, player->max_health);
 
     // 진행 상황 저장
     saveStatus(player);
