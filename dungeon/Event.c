d82fab12691cb4aa3b9db80242b2da5af32bd86a
whro1031 <whrowhro.joge@gmail.com>
2024-12-17 16:36:04 +0900

    edit event.c의 주석 수정

--- dungeon/Event.c
+++ dungeon/Event.c
@@ -8,15 +8,16 @@
 #include "shop.h"
 
 
-/*  Event name
-D = entrance dungeon
-M = shop
-B = battle
-S(칸 탐색 후 전투)?
-K = boss battle
-F = random event
-I = designated event
-*/
+// *B (Battle): 강제 전투
+// 1E (Entrance): 입구
+// *F (Force): 강제 이벤트
+// .I (Inventory): 수색 후 아이템(이나 스테이터스?) 얻는 이벤트
+// *K (King): 보스 전투
+// (L) (Location): 플레이어의 현재 위치 (사용할지는 미지수, 다른 창 넘기는 방식이면 써야 함)
+// R (Road): 수색 불가능한 길
+// .S (Search): 수색 후 전투
+// 0W (Wall): 벽. 막힘
+// *X (eXit): 출구
 
 void callEvent(char flag){
     switch(flag){
